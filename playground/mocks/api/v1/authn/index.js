7ce34ee43e52e0968ea3e9b51171b1c9c9840802
Haisheng Wu <haisheng.wu@okta.com>
2019-08-21 22:29:54 -0700

    Add mocks for quick replay issue

--- playground/mocks/api/v1/authn/index.js
+++ playground/mocks/api/v1/authn/index.js
@@ -1,8 +1,16 @@
-const data1 = require('./data/success-001.json');
+const testData = [
+  require('./data/FACTOR_ENROLL_sms.json'),
+  // require('./data/success-001.json'),
+];
+let index = 0;
 
 module.exports = {
   path: '/api/v1/authn',
   proxy: false,
   method: 'POST',
-  template: data1,
-};
+  template () {
+    if (index >= testData.length) {
+      index = 0;
+    }
+    return testData[index++];
+  },};
